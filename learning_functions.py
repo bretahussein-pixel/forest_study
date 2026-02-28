93bc1036b7c55e0e41139e9eb1588e93606606b4
Breta <bretaosodo@gmail.com>
2026-02-28 09:31:36 +0300

    The function to calculate the area of a forest has been determined

--- learning_functions.py
+++ learning_functions.py
@@ -1,9 +1,14 @@
-x = 99
+def calc_forest_area(width, length):
+    """
+    Docstring for calc_forest_area
+
+    :param width: Description
+    :param length: Description
+    """
+    area = width * length
+    return area
 
 
-def func():
-    x = 88
-
-
-call = func()
-print(call)
+nairobi_forest = calc_forest_area(300, 400)
+print(nairobi_forest)
+print(calc_forest_area.__doc__)
