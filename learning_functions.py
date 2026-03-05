a2734ea0c197cba9fe3dc8d2b00fe3ff02334317
Breta <bretaosodo@gmail.com>
2026-03-05 09:37:36 +0300

    We are trying to do the recurssion function

--- learning_functions.py
+++ learning_functions.py
@@ -1,14 +1,6 @@
-def calc_forest_area(width, length):
-    """
-    Docstring for calc_forest_area
-
-    :param width: Description
-    :param length: Description
-    """
-    area = width * length
-    return area
+def countdown(i):
+    print(i)
+    countdown(i-1)
 
 
-nairobi_forest = calc_forest_area(300, 400)
-print(nairobi_forest)
-print(calc_forest_area.__doc__)
+print(countdown(5))
